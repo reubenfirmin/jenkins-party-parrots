3a5ba3959eac70e0e84395a33e6f56c90c4e6965
reubenfirmin <rfirmin@execvision.io>
2021-08-21 05:19:43 -0600

    Update jenkins-party-parrots.js

--- src/jenkins-party-parrots.js
+++ src/jenkins-party-parrots.js
@@ -2,21 +2,25 @@ function updateBuildIcons() {
   for (const svgElement of document.getElementsByClassName("icon-blue")) { // clean build
     const newIcon = document.createElement('img');
     newIcon.src = "https://cdn.jsdelivr.net/gh/reubenfirmin/jenkins-party-parrots/images/blue.png";
+    newIcon.classList.add("parrot-build-icon");
     svgElement.parentNode.replaceChild(newIcon, svgElement);
   }
   for (const svgElement of document.getElementsByClassName("icon-red")) { // broken build
     const newIcon = document.createElement('img');
     newIcon.src = "https://cdn.jsdelivr.net/gh/reubenfirmin/jenkins-party-parrots/images/red.png";
+    newIcon.classList.add("parrot-build-icon");
     svgElement.parentNode.replaceChild(newIcon, svgElement);
   }
   for (const svgElement of document.getElementsByClassName("icon-nobuilt")) { // build with no history
     const newIcon = document.createElement('img');
     newIcon.src = "https://cdn.jsdelivr.net/gh/reubenfirmin/jenkins-party-parrots/images/nobuilt.png";
+    newIcon.classList.add("parrot-build-icon");
     svgElement.parentNode.replaceChild(newIcon, svgElement);
   }
   for (const svgElement of document.getElementsByClassName("icon-blue-anime")) { // build in progress
     const newIcon = document.createElement('img');
     newIcon.src = "https://cdn.jsdelivr.net/gh/reubenfirmin/jenkins-party-parrots/images/partyparrot.gif";
+    newIcon.classList.add("parrot-build-icon");
     svgElement.parentNode.replaceChild(newIcon, svgElement);
   }
   for (const svgElement of document.getElementsByClassName("build-status-icon__outer")) { // the rings
