e3b26f01fe3d27ddc007b2b2a29d93ec73761b2d
reubenfirmin <rfirmin@execvision.io>
2021-08-20 20:49:41 -0600

    Update jenkins-party-parrots-icons.js

--- src/jenkins-party-parrots-icons.js
+++ src/jenkins-party-parrots-icons.js
@@ -1,22 +1,22 @@
 function updateWeatherIcons() {
   for (const svgElement of document.getElementsByClassName("icon-blue")) { // clean build
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/reubenfirmin/jenkins-party-parrots/src/jenkins-party-parrots-icons.css#colour-weather-sunny";
+      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/reubenfirmin/jenkins-party-parrots/src/jenkins-party-parrots-icons.css#clean-build";
     }
   }
   for (const svgElement of document.getElementsByClassName("icon-red")) { // broken build
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/reubenfirmin/jenkins-party-parrots/src/jenkins-party-parrots-icons.css#colour-weather-partly-cloudy";
+      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/reubenfirmin/jenkins-party-parrots/src/jenkins-party-parrots-icons.css#broken-build";
     }
   }
   for (const svgElement of document.getElementsByClassName("icon-nobuilt")) { // build with no history
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/reubenfirmin/jenkins-party-parrots-icons/src/jenkins-party-parrots-icons.css#colour-weather-cloudy";
+      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/reubenfirmin/jenkins-party-parrots-icons/src/jenkins-party-parrots-icons.css#no-build";
     }
   }
   for (const svgElement of document.getElementsByClassName("icon-blue-anime")) { // build in progress
     for (const useElement of svgElement.getElementsByTagName("use")) {
-      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/reubenfirmin/jenkins-party-parrots-icons/src/jenkins-party-parrots-icons.css#colour-weather-rainy";
+      useElement.href.baseVal = "https://cdn.jsdelivr.net/gh/reubenfirmin/jenkins-party-parrots-icons/src/jenkins-party-parrots-icons.css#build-in-progress";
     }
   }
 }
